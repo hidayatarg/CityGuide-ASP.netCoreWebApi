66da00a45d4a35fc1a5356abb9050591c8929c7c
Hidayat arghandabi <hidayatarg@gmail.com>
2018-08-29 16:38:34 +0300

    Added models

---
+++ SehirRehber.API/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SehirRehber.API.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext()
+        {
+            
+        }
+        public DbSet<Value> Values { get; set; }
+    }
+}
