2b66cc976de00c5931974fb08f664e0dc14f2efa
Hidayat arghandabi <hidayatarg@gmail.com>
2018-08-31 11:10:02 +0300

    JWT Configuration Added

---
+++ SehirRehber.API/Helper/JWTExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace SehirRehber.API.Helper
+{
+    public static class  JwtExtension
+    {
+        //handle errors
+        public static void AddApplicationError(this HttpResponse response, string message)
+        {
+            // Jwt operation add to the header
+            response.Headers.Add("Application-Error",message);
+            response.Headers.Add("Access-Control-Allow-Origin","*");
+            response.Headers.Add("Access-Control-Expose-Header","Application-Error");
+        }
+    }
+}
--- SehirRehber.API/Startup.cs
+++ SehirRehber.API/Startup.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using SehirRehber.API.Data;
 
 namespace SehirRehber.API
@@ -21,12 +24,17 @@ namespace SehirRehber.API
             Configuration = configuration;
         }
 
+       
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // Web configuration place
         public void ConfigureServices(IServiceCollection services)
         {
+            // key
+            var key = Encoding.ASCII.GetBytes(Configuration.GetSection("AppSetting:Toke").Value);
+
             services.AddDbContext<DataContext>(x =>
                 x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddAutoMapper();
@@ -45,7 +53,20 @@ namespace SehirRehber.API
             //In every request create an object of
             //In case a controller request for IAppRepository Return AppRepository
             services.AddScoped<IAppRepository,AppRepository>();
-           
+
+            // Can be Used from other completed projects
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters=new TokenValidationParameters
+                {
+
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,6 +78,7 @@ namespace SehirRehber.API
             }
             //middleware //middle layer
             app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
